0105ee87cc7c688ea3dda1776da8908d9a1de5c4
TaiJespersen <tjespersen09@gmail.com>
2026-09-19 16:36:50 -0500

    Update ship.js

--- js/ship.js
+++ js/ship.js
@@ -12,7 +12,7 @@ window.AstroShip = {
   angularVelocity: new THREE.Vector3(0, 0, 0),
 
   // Flight parameters
-  maxSpeed: 120,
+  maxSpeed: 500,
   boostMultiplier: 2.2,
   thrustForce: 28,
   drag: 0.96,
