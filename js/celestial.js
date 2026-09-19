dfadec27381b6c6c638c25318af32f59402c933c
TaiJespersen <tjespersen09@gmail.com>
2026-09-19 15:57:11 -0500

    Update Nebulae

--- js/celestial.js
+++ js/celestial.js
@@ -1280,7 +1280,7 @@ window.AstroCelestial = {
           map: glowTexture,
           color: colorBase,
           transparent: true,
-          opacity: 0.008 + Math.random() * 1.4,
+          opacity: 0.008 + Math.random() * 0.14,
           blending: THREE.AdditiveBlending,
           depthWrite: false
         });
